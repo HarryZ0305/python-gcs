593e7201dbbd2c855e07e7a74e5ad9d790064a77
Harry Zhou <harryzhou935@gmail.com>
2026-05-19 21:16:03 -0700

    Create personalized custom flight modes

--- commands.py
+++ commands.py
@@ -21,3 +21,19 @@ def disarm(vehicle):
         0, 0, 0, 0, 0, 0, 0
     )
     print("Disarm command sent!")
+
+def set_mode(vehicle, mode_name):
+    print(f"Setting mode to {mode_name}...")
+    
+    if mode_name not in vehicle.mode_mapping(): # returns a dictionary of all available modes  
+        print(f"Unknown mode: {mode_name}")
+        print(f"Available modes: {list(vehicle.mode_mapping().keys())}")  
+        return
+
+    mode_id = vehicle.mode_mapping()[mode_name] # MAVLink number for the mode
+    vehicle.mav.set_mode_send(  
+        vehicle.target_system, 
+        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, # Custom flight mode flag
+        mode_id
+    )
+    print(f"Mode {mode_name} set!")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from connection import connect, request_telemetry
-from commands import arm, disarm
+from commands import arm, disarm, set_mode
 import time
 
 vehicle = connect()
 request_telemetry(vehicle)
 
+set_mode(vehicle, 'GUIDED')
+time.sleep(2)
 arm(vehicle)
 time.sleep(3)
 disarm(vehicle)
